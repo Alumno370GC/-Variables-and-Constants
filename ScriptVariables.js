2e37ffc25f084d72e30c493df649bbfb978ed2c5
Alumno370GC <alumnoitleon@gmail.com>
2024-05-27 18:35:53 -0600

    VariablesAndConstants

---
+++ ScriptVariables.js
@@ -0,0 +1,14 @@
+/**Variable names are case-sensitive. So age and Age are different variables. For example, */
+let age =23;
+let Age =20;
+
+console.log(age); //23
+console.log(Age); //23
+
+
+/**JavaScript Constants */
+/*  In JavaScript, we use the const keyword to create to constans. For example*/
+
+//asign 5 to num
+const num =5;
+console.log(num);
